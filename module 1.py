603f17fa28bcdc2f83e7e157f7d2f98d4787a20a
mihirchamp0205 <mihirchapaneri52@gmail.com>
2023-01-26 15:13:50 +0530

    First Module

---
+++ module 1.py
@@ -0,0 +1,22 @@
+1: #What is Software?
+Ans: Software is a set of computer programs and associated documentation and data. This is in contrast to hardware, from which the system is
+     built and which actully performs the work.
+
+
+2: #What are the types of Applications?
+Ans: There are 5 types of apllications
+     1 Word Processor
+     2 Spreadsheet
+     3 Presentation Program
+     4 Web browser
+     5 Enterprise Software
+
+
+3: #What is programing?
+Ans: computer programming is process of performing a particular computation, usually by designing and building an executable computer program.
+
+
+4: #what is python program?
+Ans: Python is a high-level,general-purpose programming language.
+     ist design philosophy emphasizes code readability with the
+     use of significant indentation.
